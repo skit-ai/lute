913a9a268dda403bbd103b07268f735a93e7070c
Abhinav Tushar <lepisma@fastmail.com>
2018-07-17 18:51:29 +0530

    Basic graph class

---
+++ lute/graph.py
@@ -0,0 +1,23 @@
+from typing import List, Dict, Any
+from lute.node import Node, Variable
+
+
+class Graph:
+    """
+    Class for a runnable graph
+    """
+
+    def __init__(self, inputs: List[Node], outputs: List[Node]):
+        self._inputs = inputs
+        self._outputs = outputs
+
+    def run(self, values_dict: Dict[Variable, Any]):
+        """
+        Run the values
+        """
+
+        for node in values_dict:
+            if isinstance(node, Variable) and node in self._inputs:
+                node.value = values_dict[node]
+
+        return [output.value for output in self.outputs]

1a26329515c134353de9fc65f6cd68f7dfe51e6d
Deepankar Agrawal <deepankar11093@gmail.com>
2018-07-18 20:12:30 +0530

    fix: bug in walk_node

--- lute/node/utils.py
+++ lute/node/utils.py
@@ -2,9 +2,11 @@
 Some utitlies for nodes
 """
 
-from lute.node.base import Node
 from typing import List
+
 from pydash import py_
+
+from lute.node.base import Node
 
 
 def walk_node(node: Node, backward=False) -> List[Node]:
@@ -12,7 +14,7 @@ def walk_node(node: Node, backward=False) -> List[Node]:
     Walk on the node and return a list of accessible nodes
     """
 
-    neighbours = node.predecessors if backward else successors
+    neighbours = node.predecessors if backward else node.successors
     accessible = neighbours.copy()
 
     for node in neighbours:

d954b0687542f65789350a48722c7d6a704cb8ef
Abhinav Tushar <lepisma@fastmail.com>
2018-07-18 10:15:17 +0530

    Networkx digraph creator

--- lute/graph/viz.py
+++ lute/graph/viz.py
@@ -1,3 +1,21 @@
 """
 Visualization for graph
 """
+
+import networkx as nx
+from lute.graph.graph import Graph
+
+
+def make_nx_graph(g: Graph) -> nx.DiGraph:
+    dg = nx.DiGraph()
+
+    for n in g.inputs + g.outputs:
+        dg.add_node(n)
+
+    for n in g.outputs:
+        for in_n in n.predecessors:
+            if in_n not in dg.nodes:
+                dg.add_node(in_n)
+            dg.add_edge(in_n, n)
+
+    return dg

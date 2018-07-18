e3e41ba3219a3c4f5149fc93f74541be3343ac99
Abhinav Tushar <lepisma@fastmail.com>
2018-07-18 17:31:05 +0530

    Fix pruning to remove dangling nodes

--- lute/graph/optimize.py
+++ lute/graph/optimize.py
@@ -10,9 +10,14 @@ from pydash import py_
 
 def prune(g: Graph) -> Graph:
     """
-    Clear inputs which are not needed.
+    Clear inputs which are not needed. Remove dangling leaf nodes.
     """
 
-    required = py_.flatten([walk_node(out, backward=True) for out in g.outputs])
+    required_inputs = [i for i in g.inputs if i in g._backward_nodes()]
 
-    return Graph([i for i in g.inputs if i in required], outputs)
+    for node in g._forward_nodes():
+        if node not in outputs and len(node.successors) == 0:
+            for pnode in node.predecessors:
+                pnode.successors.remove(node)
+
+    return Graph(required_inputs, outputs)

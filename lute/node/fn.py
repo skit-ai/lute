24eca1e9f9c4e645a5d6c4ac1d614a2796d2c530
Abhinav Tushar <lepisma@fastmail.com>
2018-07-21 03:48:29 +0530

    Partial support for function node decorator

---
+++ lute/node/fn.py
@@ -0,0 +1,58 @@
+"""
+Function to node transformer
+"""
+
+import ast
+import inspect
+
+from lute.node.base import Node, NodeMeta
+
+
+class ValueTransformer(ast.NodeTransformer):
+    """
+    Change desired variables to variables.value
+    """
+
+    def __init__(self, input_ids):
+        super().__init__()
+        self._input_ids = input_ids
+
+    def visit_Name(self, node):
+        if node.id in self._input_ids:
+            return ast.Call(
+                func=ast.Name(id="getattr", ctx=ast.Load()),
+                args=[node, ast.Str("value")],
+                keywords=[]
+            )
+        else:
+            return node
+
+
+def fn_node(fn) -> Node:
+    """
+    Convert the function to a node
+    """
+
+    name = fn.__name__
+    args = inspect.getfullargspec(fn).args
+
+    # Patch function
+    tree = ast.parse(inspect.getsource(fn))
+    tree = ValueTransformer(args).visit(tree)
+    ast.fix_missing_locations(tree)
+
+    namespace = {}
+    exec(compile(tree, filename="<ast>", mode="exec"), namespace)
+
+    def __init__(self):
+        pass
+
+    def __call__(self, *args):
+        self._input = args
+        return self
+
+    return NodeMeta(name, (Node,), {
+        "__init__": __init__,
+        "__call__": __call__,
+        "eval": lambda self: namespace[name](*self._input)
+    })

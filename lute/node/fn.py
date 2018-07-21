e41424178f84c2e9a4af56ca2ad50534537c8af3
Abhinav Tushar <lepisma@fastmail.com>
2018-07-21 23:25:33 +0530

    Add nametransformer and fix scope issue

--- lute/node/fn.py
+++ lute/node/fn.py
@@ -4,8 +4,32 @@ Function to node transformer
 
 import ast
 import inspect
+from uuid import uuid4
 
 from lute.node.base import Node, NodeMeta
+
+
+class NameTransformer(ast.NodeTransformer):
+    """
+    Change the name of function (in definition)
+    """
+
+    def __init__(self, old_name, new_name):
+        super().__init__()
+        self.old_name = old_name
+        self.new_name = new_name
+
+    def visit_FunctionDef(self, node):
+        if node.name == self.old_name:
+            return ast.FunctionDef(
+                name=self.new_name,
+                args=node.args,
+                body=node.body,
+                decorator_list=node.decorator_list,
+                returns=node.returns
+            )
+        else:
+            return node
 
 
 class ValueTransformer(ast.NodeTransformer):
@@ -28,21 +52,29 @@ class ValueTransformer(ast.NodeTransformer):
             return node
 
 
+def unique_name(root: str) -> str:
+    return "{}__{}".format(root, str(uuid4()).replace("-", "_"))
+
+
 def fn_node(fn) -> Node:
     """
     Convert the function to a node
     """
 
     name = fn.__name__
+    global_namespace = fn.__globals__
     args = inspect.getfullargspec(fn).args
+
+    # Assign new name to avoid collision
+    new_name = unique_name(name)
 
     # Patch function
     tree = ast.parse(inspect.getsource(fn))
     tree = ValueTransformer(args).visit(tree)
+    tree = NameTransformer(name, new_name).visit(tree)
     ast.fix_missing_locations(tree)
 
-    namespace = {}
-    exec(compile(tree, filename="<ast>", mode="exec"), namespace)
+    exec(compile(tree, filename="<ast>", mode="exec"), global_namespace)
 
     def __init__(self):
         pass
@@ -52,13 +84,8 @@ def fn_node(fn) -> Node:
         self._input = args
         return self
 
-    Cls = NodeMeta(name, (Node,), {
+    return NodeMeta(unique_name(name), (Node,), {
         "__init__": __init__,
         "__call__": __call__,
-        "eval": lambda self: namespace[name](*self._input)
+        "eval": lambda self: global_namespace[new_name](*self._input)
     })
-
-    def _wrapper(*args):
-        return Cls(*args)
-
-    return _wrapper

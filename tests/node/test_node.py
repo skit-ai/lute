99bb7c1708dbfdac1815339854d1fddbcefe3d83
Abhinav Tushar <lepisma@fastmail.com>
2018-07-25 19:21:24 +0530

    More tests for fn_node fix

--- tests/node/test_node.py
+++ tests/node/test_node.py
@@ -2,6 +2,7 @@ from lute.graph import Graph
 from lute.node import Constant, Variable
 from lute.node.fn import fn_node
 
+
 def test_id():
     Constant._count = -1
     Variable._count = -1
@@ -21,6 +22,24 @@ def test_id():
 def what_func(a, b, c):
     a = b+c
     return a
+
+def which_func(a):
+    a = a + 1
+    return a
+
+def test_func_node_replacement():
+    x = Constant(2)
+    g = Graph(x, x >> fn_node(which_func)())
+    assert g.run() == 34
+
+def test_something():
+    x = Variable()
+
+    y = x >> fn_node(which_func)()
+    z = x >> fn_node(which_func)()
+    g = Graph(x, [y, z])
+    y_out, z_out = g.run({ x: 10 })
+    assert y_out == z_out
 
 def test_func_node():
     x = Variable()

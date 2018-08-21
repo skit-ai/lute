8c66cfddcaba8916f58909d40724f53dd1f9ad7e
Abhinav Tushar <lepisma@fastmail.com>
2018-08-21 13:22:54 +0530

    Robuster node resolution

--- lute/exceptions.py
+++ lute/exceptions.py
@@ -4,3 +4,13 @@ class TypeMismatchException(Exception):
     def __init__(self, expected_type, got_type, *args, **kwargs):
         message = "Type Mismatch: Expected %s got %s" % (expected_type, got_type)
         super().__init__(message, *args, **kwargs)
+
+
+class ResolutionException(Exception):
+    """
+    Exception for resolving a node from a list
+    """
+
+    def __init__(self, node_id, *args, **kwargs):
+        message = f"Unable to resolve {node_id}"
+        super().__init__(message, *args, **kwargs)
